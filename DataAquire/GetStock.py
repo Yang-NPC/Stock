6e6cb45716beaed3b3b4fc528b48bf4b391a4823
Zhiyang Cheng <guoguo54a1@163.com>
2023-12-02 23:40:24 -0500

    get all requiredstock data and create the folder for each model

--- DataAquire/GetStock.py
+++ DataAquire/GetStock.py
@@ -5,17 +5,19 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
-start_date = '2023-01-01' 
-end_date = '2023-01-31'
+start_date = '2014-07-01' 
+end_date = '2015-06-01'
+
 
 #open the file
-with open('NIFTY-50-Name.csv', mode='r') as file:
+with open('MW-NIFTY-50-26-Oct-2023.csv', mode='r') as file:
     reader = csv.reader(file)
     first_column = [row[0] for row in reader if row]  
 
 #get the stock name and paresing array 
 stock_name = first_column[first_column.index('TATASTEEL'):] 
 for i in range(len(stock_name)):
+    #make sure there is .NS at the end for NIFTY-50
     stock_name[i] = stock_name[i] + '.NS'
     
 print(stock_name)
@@ -24,6 +26,7 @@ save_dir = 'DataAquire/StockData'
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)  # Create the directory if it doesn't exist
 
+#write the data that download from yfinance to the cvs file
 for ticker_symbol in stock_name:
     stock_data = yf.download(ticker_symbol, start=start_date, end=end_date)
     print(f"Stock Data for {ticker_symbol}:")
@@ -35,7 +38,7 @@ for ticker_symbol in stock_name:
     stock_data.to_csv(csv_filepath)
     
 
-# Save the data to a CSV file in the specified directory
+
 
 
     
